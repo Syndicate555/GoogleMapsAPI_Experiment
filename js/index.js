ffb7fe7832bdfef782a447d81970992c9f79b80b
Syndicate555 <saziz028@uottawa.ca>
2020-05-28 18:34:18 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 function initMap() {
-  var sydney = {
-    lat: -33.863276,
-    lng: 151.107977,
+  var ottawa = {
+    lat: 45.4215,
+    lng: -75.6972,
   };
   // Create the map.
   const map = new google.maps.Map(document.getElementById("map"), {
     mapTypeId: "roadmap",
     zoom: 11,
-    center: sydney,
+    center: ottawa,
   });
 }
