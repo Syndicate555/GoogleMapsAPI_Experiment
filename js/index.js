d525f2c0a9c35175590c8e799fc8cb413b67c5de
Syndicate555 <saziz028@uottawa.ca>
2020-05-29 07:42:50 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -58,7 +58,7 @@ function showStoresMarkers() {
   map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, statusText,phone) {
+function createMarker(latlng, name, address, statusText, phone) {
   var html = `<div class = "store-info-window">
            <div class = "store-info-name">
            ${name}
@@ -70,7 +70,7 @@ function createMarker(latlng, name, address, statusText,phone) {
            ${address}
            </div>
            <div class = "store-info-phone">
-           ${}
+           ${phone}
            </div>
         </div>         
   
