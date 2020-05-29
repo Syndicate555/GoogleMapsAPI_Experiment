2024561dce059d7b1bfade0189e82c9734774b0a
Syndicate555 <saziz028@uottawa.ca>
2020-05-28 22:20:44 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,70 @@
+var map;
+var markers = [];
+var infoWindow;
 function initMap() {
-  var ottawa = {
-    lat: 45.4215,
-    lng: -75.6972,
+  var losAngeles = {
+    lat: 34.06338,
+    lng: -118.35808,
   };
-  // Create the map.
-  const map = new google.maps.Map(document.getElementById("map"), {
-    mapTypeId: "roadmap",
-    zoom: 11,
-    center: ottawa,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: losAngeles,
+    zoom: 8,
   });
+  infoWindow = new google.maps.InfoWindow();
+  displayStores();
+  showStoresMarkers();
+}
+
+function displayStores() {
+  var storesHtml = "";
+  stores.forEach(function (store, index) {
+    var address = store.addressLines;
+    var phone = store.phoneNumber;
+    storesHtml += `
+            <div class="store-container">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>${address[0]}</span>
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="store-phone-number">${phone}</div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        ${index + 1}
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+  document.querySelector(".stores-list").innerHTML = storesHtml;
+}
+
+function showStoresMarkers() {
+  var bounds = new google.maps.LatLngBounds();
+  stores.forEach(function (store, index) {
+    var latlng = new google.maps.LatLng(
+      store.coordinates.latitude,
+      store.coordinates.longitude
+    );
+    console.log(latlng);
+    var name = store.name;
+    var address = store.addressLines[0];
+    bounds.extend(latlng);
+    createMarker(latlng, name, address);
+  });
+  map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address) {
+  var html = "<b>" + name + "</b> <br/>" + address;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+  });
+  google.maps.event.addListener(marker, "click", function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
 }
